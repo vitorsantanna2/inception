e27715aa4e3e2612fa44eb13386ab415d81c348c
jsantann <jsantann@jsantann.42.fr>
2024-01-09 16:36:33 -0300

    finish inception

--- srcs/requirements/nginx/tools/genSSL.sh
+++
@@ -1,8 +0,0 @@
-#import ssl certificate
-echo "-----BEGIN CERTIFICATE-----" > /etc/ssl/certs/certified.crt
-echo $SSIGNEDCRT >> /etc/ssl/certs/certified.crt
-echo -n "-----END CERTIFICATE-----" >> /etc/ssl/certs/certified.crt
-
-echo "-----BEGIN PRIVATE KEY-----" > /etc/ssl/private/private.key
-echo $SSIGNEDKEY >> /etc/ssl/private/private.key
-echo -n "-----END PRIVATE KEY-----" >> /etc/ssl/private/private.key
