33d99e406473bd81d3cf17e3bc68da6fb0588953
Vitor Santanna <vitor_anna@id.uff.br>
2024-01-11 19:53:10 -0300

    change startwp

--- srcs/requirements/wordpress/conf/startwp.sh
+++ srcs/requirements/wordpress/conf/startwp.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 if [ ! -f "/var/www/wp-config.php" ]; then
-	wp config create --dbname=${DB_NAME} --dbuser=${DB_USER} --dbpass=${DB_PASS} --dbhost=mariadb
+	wp core download
+	wp config create --dbname=${DB_NAME} --dbuser=${DB_USER} --dbpass=${DB_PASS} --dbhost=mariadb 
+	echo "define( 'WP_HOME', 'https://jsantann.42.fr' );" >> /var/www/wp-config.php 
+	echo "define( 'WP_SITEURL', 'https://jsantann.42.fr');" >> /var/www/wp-config.php
 	wp core install --url=jsantann.42.fr --title=Inception --admin_user=${WP_ADMIN} --admin_password=${WP_ADMIN_PASS} --admin_email=${WP_ADMIN_EMAIL}
 	wp user create ${WP_USER} ${WP_USER_EMAIL} --role=editor
 fi
