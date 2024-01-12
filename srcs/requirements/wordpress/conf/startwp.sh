4959bece9faa651361d9c1100d6ec584963085d8
Vitor Santanna <vitor_anna@id.uff.br>
2024-01-12 14:51:14 -0300

    wordpress working

--- srcs/requirements/wordpress/conf/startwp.sh
+++ srcs/requirements/wordpress/conf/startwp.sh
@@ -2,9 +2,7 @@
 if [ ! -f "/var/www/wp-config.php" ]; then
 	wp core download
 	wp config create --dbname=${DB_NAME} --dbuser=${DB_USER} --dbpass=${DB_PASS} --dbhost=mariadb 
-	echo "define( 'WP_HOME', 'https://jsantann.42.fr' );" >> /var/www/wp-config.php 
-	echo "define( 'WP_SITEURL', 'https://jsantann.42.fr');" >> /var/www/wp-config.php
 	wp core install --url=jsantann.42.fr --title=Inception --admin_user=${WP_ADMIN} --admin_password=${WP_ADMIN_PASS} --admin_email=${WP_ADMIN_EMAIL}
-	wp user create ${WP_USER} ${WP_USER_EMAIL} --role=editor
+	wp user create ${WP_USER}  ${WP_USER_EMAIL}
 fi
 /usr/sbin/php-fpm82 -F
